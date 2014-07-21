09f7d830ae440d0ee410b18898b08c0107354ebc
Kristiyan <zontak@mail.bg>
2014-07-22 01:03:04 +0300

    update

--- JavaScript Third Homework/15.MostFrequendWord/frequentWord.js
+++ JavaScript Third Homework/15.MostFrequendWord/frequentWord.js
@@ -1,52 +1,36 @@
-﻿console.log(friquentWord('in the middle of the night'));
-console.log('\n');
-console.log(friquentWord('Welcome to SoftUni. Welcome to Java. Welcome everyone.'));
-console.log('\n');
-console.log(friquentWord('Hello my friend, hello my darling. Come on, come here. Welcome, welcome darling.'));
-function friquentWord(string) {
-    var words = string.split(' ');
-    var wordsCount = words.length;
-    var wordsArr = [];
-    for (var i = 0; i < wordsCount; i++) {
-        var result = popComma(words[i]);
-        if (wordsArr[result] == undefined) {
-            wordsArr[result] = 1;
+﻿friquentWord('in the middle of the night');
+friquentWord('Welcome to SoftUni. Welcome to Java. Welcome everyone.');
+friquentWord('Hello my friend, hello my darling. Come on, come here. Welcome, welcome darling.');
+function friquentWord(arr) {
+    arr = arr.toLowerCase();
+    var words = arr.split(/\W+/);
+    var occurances = [];
+    var index = 0;
+    for (var m = 0; m < words.length; m++) {
+        var flag = false;
+        for (var i = 0; i < occurances.length; i++) {
+            if (words[m] == occurances[i][0]) {
+                occurances[i][1]++;
+                flag = true;
+                break;
+            }
         }
-        else {
-            wordsArr[result] += 1;
-        }
-    }
-    return whichAreTheMost(wordsArr);
-}
-function popComma(word) {
-    var alphabetic = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    var bool = true;
-    var lastSymbol = word[word.length - 1];
-    for (var j = 0; j < alphabetic.length; j++) {
-        if (lastSymbol != alphabetic[j]) {
-            bool = false;
-        }
-        else {
-            return word.toLowerCase();
+        if (!flag) {
+            occurances[index] = [];
+            occurances[index][0] = words[m];
+            occurances[index][1] = 1;
+            index++;
         }
     }
-    if (bool == false) {
-        word = word.substr(0, word.length - 1);
-        return word.toLowerCase();
-    }
-}
-function whichAreTheMost(arr) {
-    var result = '';
-    var counter = 0;
-    for (var index in arr) {
-        var dinamicCounter = arr[index];
-        if (dinamicCounter > counter) {
-            counter = dinamicCounter;
-            result = index + ' -> ' + arr[index];
-        }
-        else if (dinamicCounter == counter && result != '') {
-            result += '\n' + index + ' -> ' + arr[index];
+    var mostFrequend = 0;
+    for (var j = 0; j < occurances.length; j++) {
+        if (mostFrequend < occurances[j][1]) {
+            mostFrequend = occurances[j][1];
         }
     }
-    return result;
+    for (var i = 0; i < occurances.length; i++) {
+        if (mostFrequend == occurances[i][1]) {
+            console.log(occurances[i][0] + ' -> ' + occurances[i][1] + ' times' );
+        }
+    }
 }
--- JavaScript Third Homework/16.CardsFrequencies/cardFrequencies.js
+++ JavaScript Third Homework/16.CardsFrequencies/cardFrequencies.js
@@ -1,52 +1,28 @@
 ﻿cardFriquency('8♥ 2♣ 4♦ 10♦ J♥ A♠ K♦ 10♥ K♠ K♦');
-console.log('\n');
 cardFriquency('J♥ 2♣ 2♦ 2♥ 2♦ 2♠ 2♦ J♥ 2♠');
-console.log('\n');
 cardFriquency('10♣ 10♥');
-function cardFriquency(string) {
-    var cardsWithFace = string.split(' ');
-    var cardsWithowtFace = [];
-    var handLength = cardsWithFace.length;
-    for (var i = 0; i < handLength; i++) {
-        cardsWithowtFace[i] = smashTheFace(cardsWithFace[i]);
-    }
-    return collectFriquency(cardsWithowtFace, handLength);
-}
-function smashTheFace(card) {
-    if (card[0] == 1) {
-        card = card.substring(0, 2);
-    }
-    else {
-        card = card.substring(0, 1);
-    }
-    return card.toLowerCase();
-}
-function collectFriquency(arr, length) {
-    var friquencyArr = [];
-    for (var j = 0; j < length; j++) {
-        if (friquencyArr[arr[j]] == undefined) {
-            friquencyArr[arr[j]] = 1;
-        }
-        else {
-            friquencyArr[arr[j]] += 1;
-        }
-    }
-    return mathFriquency(friquencyArr, length, arr);
-}
-function mathFriquency(arr, length, realArr) {
-    for (var index in arr) {
-        arr[index] = (arr[index] / length) * 100;
-        arr[index] = arr[index].toFixed(2) + '%';
-    }
-    return orderAndOutPut(arr, realArr);
-}
-function orderAndOutPut(unorderedArr, realArr) {
-    for (var i in realArr) {
-        for (var j in unorderedArr) {
-            if (realArr[i] == j && unorderedArr[j] != undefined) {
-                console.log(j.toUpperCase() + ' -> ' + unorderedArr[j]);
-                unorderedArr[j] = undefined;
+
+function cardFriquency(string) {    
+    var numbers = string.split(/\W+/);
+    var array = [];
+    var counter = 0;
+    for (var i = 0; i < numbers.length -1; i++) {
+        var flag = false;
+        for (var m = 0; m < array.length; m++) {
+            if (numbers[i] == array[m][0]) {
+                flag = true;
+                array[m][1]++;
             }
         }
+        if (flag == false) {
+            array[counter] = [];
+            array[counter][0] = numbers[i];
+            array[counter][1] = 1;
+            counter++;
+        }
+    }
+    for (var i = 0; i < array.length; i++) {
+        var percent = (array[i][1] * ((numbers.length - 1) / 100) *100).toFixed(2);
+        console.log(array[i][0] + ' -> ' + percent + '%');
     }
 }
